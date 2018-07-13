c90ab68ddfb2532d2b9a6545acc2cb09bdee4996
Guy Harwood <guy@milkchip.com>
2018-07-13 19:34:59 +0100

    add moment

--- urlfunc/index.js
+++ urlfunc/index.js
@@ -1,4 +1,7 @@
+const moment = require('moment')
+
 module.exports = function (context, req) {
   context.log('Node JS function deployed by VSTS.')
+  context.log(`current datetime is ${moment()}`)
   context.done()
 }
